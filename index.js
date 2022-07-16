44b88a87496e601783d086063e256ce4ec203cb8
Clarky <clarky117@outlook.com>
2022-07-16 13:56:43 +0800

    files created, small pseudo code written

---
+++ index.js
@@ -0,0 +1,6 @@
+// import generateHTML and inquirer etc
+// use filesystem to export with __dirname
+
+// ask questions using inquirer
+
+// when i receive data from the questions, use them to populate my template
---
+++ utils/generate-html.js
@@ -0,0 +1,9 @@
+// create a function to generate a html file from the template i make
+
+function generateHTML(){
+
+}
+
+// export function
+
+module.exports = generateHTML;
